ebd79fd583a2f9d051937375f895709a85004f0c
pakupek <104196200+pakupek@users.noreply.github.com>
2022-04-25 18:42:50 +0200

    update lista_zakupow.py

--- lista_zakupow.py
+++ lista_zakupow.py
@@ -8,12 +8,12 @@
 #Dodatkowo, używając wbudowanych metod operacji na napisach, spowoduj, aby nazwy sklepów i towarów były wypisane wielką literą (sięgnij do oficjalnej dokumentacji, by odnaleźć taką funkcjonalność).
 #Na koniec, w ostatniej linii wypisz W sumie kupuję <X> produktów.. X to sumaryczna liczba towarów, które są na listach.
 #Twój program po uruchomieniu, powinien wyświetlić następujące informacje:
-'''
-Lista zakupów
-Idę do Piekarnia, kupuję tu następujące rzeczy: ['Chleb', 'Pączek', 'Bułki'].
-Idę do Warzywniak, kupuję tu następujące rzeczy: ['Marchew', 'Seler', 'Rukola'].
-W sumie kupuję 6 produktów.
-'''
+
+#Lista zakupów
+#Idę do Piekarnia, kupuję tu następujące rzeczy: ['Chleb', 'Pączek', 'Bułki'].
+#Idę do Warzywniak, kupuję tu następujące rzeczy: ['Marchew', 'Seler', 'Rukola'].
+#W sumie kupuję 6 produktów.
+
 shopping_dict = {'Piekarnia':['chleb','bułki','pączek'], 'Warzywniak': ['marchew','seler','rukola']}
 print('Lista zakupów')
 
